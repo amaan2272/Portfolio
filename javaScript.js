fc5f64ecab67ef7007e470cf74e4b9f246038627
Amaan Khan <amaankhan2272@gmail.com>
2025-05-28 12:51:09 +0530

    first commit

---
+++ javaScript.js
@@ -0,0 +1,23 @@
+// ARITHEMATIC OPERATORS
+
+a = 5;
+b = 4;
+
+// console.log("a =", a,"b =", b);
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+// console.log(a++);
+// console.log(a)
+// console.log(a--)
+// console.log(a)
+
+// ASSIGNMENT OPERATOR
+
+console.log(a += 5) 
+console.log(a -= 5)
+console.log(a * b)
+
